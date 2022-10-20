2abe3411eb80e87ee2bc46bdfd98e09dc8acd014
Mayuri Dalavai <mayuri.gj@gmail.com>
2022-10-20 07:01:31 -0700

    added newgame and exit, AB#193, AB#192

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,26 +4,19 @@ import com.company.models.Home;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.*;
+
+import com.apps.util.Prompter;
+
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("\n" +
-                "\n" +
-                " _______    _______ _________ _______  _______ _________ _______  _  _______   _________ _______  _        _______ \n" +
-                "(  ___  )  (  ____ )\\__   __/(  ____ )(  ___  )\\__   __/(  ____ \\( )(  ____ \\  \\__   __/(  ___  )( \\      (  ____ \\\n" +
-                "| (   ) |  | (    )|   ) (   | (    )|| (   ) |   ) (   | (    \\/|/ | (    \\/     ) (   | (   ) || (      | (    \\/\n" +
-                "| (___) |  | (____)|   | |   | (____)|| (___) |   | |   | (__       | (_____      | |   | (___) || |      | (__    \n" +
-                "|  ___  |  |  _____)   | |   |     __)|  ___  |   | |   |  __)      (_____  )     | |   |  ___  || |      |  __)   \n" +
-                "| (   ) |  | (         | |   | (\\ (   | (   ) |   | |   | (               ) |     | |   | (   ) || |      | (      \n" +
-                "| )   ( |  | )      ___) (___| ) \\ \\__| )   ( |   | |   | (____/\\   /\\____) |     | |   | )   ( || (____/\\| (____/\\\n" +
-                "|/     \\|  |/       \\_______/|/   \\__/|/     \\|   )_(   (_______/   \\_______)     )_(   |/     \\|(_______/(_______/\n" +
-                "                                                                                                                   \n" +
-                "\n");
+        Home home = new Home();
+        home.buildHome();
 
-        System.out.println("This is a terminal based role playing game. Seek adventure, by solving the mystery of the " +
-                "secret treasure of Skull Island. Please select a MENU option to continue.\n");
 
-        System.out.println("MENU:   New Game  |  Exit  ");
-        
+
     }
+
 }
--- src/com/company/models/Home.java
+++ src/com/company/models/Home.java
@@ -1,5 +1,56 @@
 package com.company.models;
 
+import com.apps.util.Prompter;
+
+import java.util.Scanner;
+import java.util.*;
+
 public class Home {
 
+    //Fields
+    private String playerName;
+    Prompter prompter =  new Prompter(new Scanner(System.in));
+
+//Methods
+
+public void buildHome(){
+   banner();
+   gameInfo();
+   startGame();
+}
+private void banner() {
+    System.out.println("\n" +
+            "\n" +
+            " _______    _______ _________ _______  _______ _________ _______  _  _______   _________ _______  _        _______ \n" +
+            "(  ___  )  (  ____ )\\__   __/(  ____ )(  ___  )\\__   __/(  ____ \\( )(  ____ \\  \\__   __/(  ___  )( \\      (  ____ \\\n" +
+            "| (   ) |  | (    )|   ) (   | (    )|| (   ) |   ) (   | (    \\/|/ | (    \\/     ) (   | (   ) || (      | (    \\/\n" +
+            "| (___) |  | (____)|   | |   | (____)|| (___) |   | |   | (__       | (_____      | |   | (___) || |      | (__    \n" +
+            "|  ___  |  |  _____)   | |   |     __)|  ___  |   | |   |  __)      (_____  )     | |   |  ___  || |      |  __)   \n" +
+            "| (   ) |  | (         | |   | (\\ (   | (   ) |   | |   | (               ) |     | |   | (   ) || |      | (      \n" +
+            "| )   ( |  | )      ___) (___| ) \\ \\__| )   ( |   | |   | (____/\\   /\\____) |     | |   | )   ( || (____/\\| (____/\\\n" +
+            "|/     \\|  |/       \\_______/|/   \\__/|/     \\|   )_(   (_______/   \\_______)     )_(   |/     \\|(_______/(_______/\n" +
+            "                                                                                                                   \n" +
+            "\n");
+}
+
+private void gameInfo() {
+    System.out.println("This is a terminal based role playing game. Seek adventure, by solving the mystery of the " +
+            "secret treasure of Skull Island. Please select a MENU option to continue.\n");
+
+
+}
+
+    private void startGame(){
+    setMenuSelection(prompter.prompt("MENU:   New Game  |  Exit"));
+}
+
+    private void setMenuSelection(String menuSelection) {
+    if(menuSelection.toLowerCase() == "new game"){
+          //ToDo: Method to create new game
+
+    }
+    if(menuSelection.toLowerCase() == "exit"){
+         System.exit(0);
+    }
+    }
 }
